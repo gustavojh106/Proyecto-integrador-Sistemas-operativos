5b285c325bb00b6d9f922f6361d148f5ec4d9551
gustavojh106 <51544403+gustavojh106@users.noreply.github.com>
2020-04-12 13:33:48 -0600

    Add files via upload

---
+++ semaforo.h
@@ -0,0 +1,29 @@
+/*
+   C++ class to encapsulate Unix semaphore intrinsic structures and
+   system calls
+*/
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+#include <iostream>
+#include <string>
+
+#define KEY 0xA12345	// Valor de la llave del recurso
+
+using namespace std;
+
+class Semaforo {
+   public:
+      Semaforo( int ValorInicial = 0 );
+      ~Semaforo();
+      void Signal();	// Puede llamarse V
+      void Wait();	// Puede llamarse P
+	  union semun{//STRUCT PARA SEMCTL
+        int val;
+		struct semid_ds *buf;
+		ushort *array;
+        struct seminfo *_buf;
+	  };
+      int id;		// Identificador del semaforo
+};
