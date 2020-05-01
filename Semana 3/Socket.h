a1fe83cd205818f877c06215b7c6647e457f34b3
gustavojh106 <51544403+gustavojh106@users.noreply.github.com>
2020-04-30 18:41:13 -0600

    Add files via upload

---
+++ Semana 3/Socket.h
@@ -0,0 +1,32 @@
+/*
+ *  Establece la definicion de la clase Socket para efectuar la comunicacion
+ *  de procesos que no comparten memoria, utilizando un esquema de memoria
+ *  distribuida.  El desarrollo de la clase se hara en dos etapas, primero
+ *  los metodos necesarios para los clientes, en la otra etapa los otros
+ *  metodos para los servidores
+ */
+
+#ifndef Socket_h
+#define Socket_h
+#include <netinet/in.h>
+#include <string.h>
+
+class Socket{
+
+    public:
+        Socket( char, bool = false );
+        Socket( int ); //solicita un socket al sistema operativo
+        ~Socket();
+        int Connect( const char *, int ); //para ipv4
+        int Connect( const char *, char * ); // para ipv6
+        void Close();
+        int Read( const char* , int ); // lee los datos del servidor y los guarda
+        int Write( const char* , int ); //pide una solicitud al servidor para poder leer
+        
+    private:
+
+        int idSocket;
+        bool ipv6;
+};
+
+#endif
